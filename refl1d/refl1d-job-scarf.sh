863123b13cf1f2ef9c66c5eec166fcd0ad0f283e
Jounaid <jounaidruhomaun@googlemail.com>
2025-11-18 14:58:06 +0000

    Revise job script for refl1d

--- refl1d/refl1d-job-scarf.sh
+++ refl1d/refl1d-job-scarf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=refl1dTest   # create a name for your job
-#SBATCH --nodes=2                # node count
+#SBATCH --job-name=refl1dTest    # job name
+#SBATCH --nodes=4                # node count
 
 #SBATCH --time=00:10:00          # total run time limit (HH:MM:SS)
 #SBATCH --output refl1dTest-%j.out
@@ -8,8 +8,5 @@
 #SBATCH --ntasks-per-node=4
 
 module purge
-#module load MPICH
-#module load mpi4py/1.3
-module load contrib/dls-ap/intel/mpi/latest
 
-mpirun -np 8 singularity exec --bind /home/vol07/scarf1137/jounaidr/ada:/test /home/vol07/scarf1137/jounaidr/ada/mpi4py-mpich-refl1d.sif python3.11 /test/align_magnetic_test.py
+singularity exec --bind /home/vol07/scarf1137/jounaidr/ada:/test /home/vol07/scarf1137/jounaidr/ada/mpi4py-mpich-refl1d.sif refl1d /test/Ni58_basic_model.py --fit=dream --export=test --burn=1000 --samples=10000 --init=lhs --mpi --session=/test/test.h5 --batch
